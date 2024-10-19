f550e9efa1c2cc3dc230b479db8d097055392e20
ameell08 <amelia.safitri.wahyu@gmail.com>
2024-10-19 21:10:26 +0700

    pertemuan 8 - praktikum 3

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -8,6 +8,7 @@ use App\Models\BarangModel;
 use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class BarangController extends Controller
 {
@@ -374,4 +375,21 @@ class BarangController extends Controller
         $writer->save('php://output');
         exit;
     } // end function export excel
+
+    public function export_pdf(){
+        //ambil data barang yang akan di export pdf
+        $barang = BarangModel::select('kategori_id', 'barang_kode','barang_nama','harga_beli','harga_jual')
+        ->orderBy('kategori_id')
+        ->orderBy('barang_kode')
+        ->with('kategori')
+        ->get();
+        //use Barryvdh\domPDF\Facecade\PDF
+        $pdf = Pdf::loadView('barang.export_pdf',['barang' => $barang]);
+        $pdf->setPaper('a4','portrait'); //set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); //Set true jika ada gambar dari url
+        $pdf->render();
+
+        return $pdf->stream('Data Barang'. date('Y-m-d H:i:s'). '.pdf');
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -83,7 +83,8 @@ use App\Http\Controllers\WelcomeController;
                     Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);       //menghapus data barang AJAX
                     Route::get('/import', [BarangController::class, 'import']);      // ajax form upload excel
                     Route::post('/import_ajax', [BarangController::class, 'import_ajax']);      // ajax import excel
-                    Route::get('/export_excel',[BarangController::class, 'export_excel']);
+                    Route::get('/export_excel',[BarangController::class, 'export_excel']);      
+                    Route::get('/export_pdf',[BarangController::class,'export_pdf']); // export pdf
                 });
             });
 
